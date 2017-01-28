781b77a853a3ec9216fe1bb7834a31d75842c52e
Ashok Siyani <ashok.siyani@gmail.com>
2017-01-28 13:57:02 +0000

    Holiday API completed

--- controllers/api/holiday.js
+++ controllers/api/holiday.js
@@ -1,48 +1,109 @@
 const express = require('express');
+const _ = require('lodash');
 const {Holidays} = require('./../../models/holiday');
+const {Users} = require('./../../models/user');
 
 const router = express.Router();
 
-//Get all users by department -> return users = json
+//Search Holiday -> return holidays = json
 router.get('/search/', (req,res) => {
-    let searchQuery = _.pick(req.query,["staffName","department","startDate","endDate"]);
+    let searchQuery = _.pick(req.query,["staff_id","department","fromDate","toDate"]);
     
 	
-// .query.filter(models.holiday_table.department == request.get_json()['department'],
-// (( models.holiday_table.start.between(fromDate,toDate) ) OR
-// ((models.holiday_table.start <= fromDate) & (models.holiday_table.end >= fromDate)) )) 
-	
+    // .query.filter(models.holiday_table.department == request.get_json()['department'],
+    // (( models.holiday_table.start.between(fromDate,toDate) ) OR
+    // ((models.holiday_table.start <= fromDate) & (models.holiday_table.end >= fromDate)) )) 
+	let dbQuery = {};
+    
+    if(_.has(searchQuery,'staff_id') )
+         dbQuery.staff_id = searchQuery.staff_id ;
+    if(_.has(searchQuery,'department') )
+        dbQuery.department = searchQuery.department ;
 
-    if(searchQuery.hasOwnProperty("startDate") && searchQuery.hasOwnProperty("endDate")){
+    if(searchQuery.hasOwnProperty("fromDate") && searchQuery.hasOwnProperty("toDate")){
         
-        Holidays.find({
-
-        })
-
-    }else{
-
+        dbQuery.$or =[{startDate:{ $gt:searchQuery.fromDate , $lt:searchQuery.toDate }},
+                      {$and:[ {startDate:{$lt:searchQuery.fromDate}}, {endDate:{$lt:searchQuery.fromDate }} ] }];
+        
+        //console.log(JSON.stringify(dbQuery,undefined,2));
+    }else if(searchQuery.hasOwnProperty("fromDate") || searchQuery.hasOwnProperty("toDate")){
+        return res.status(400).send('Invalid query need both startDate and endDate');
     }
-    res.send("OK");
+
+    //Make sure dbQuery got a value
+    if(_.isEmpty(dbQuery)){
+        return res.status(400).send('Invalid query');
+    }
+
+            Holidays.find(dbQuery).then( (holidays) => {
+                                 res.send({holidays});
+                                })
+                              .catch( e => res.status(400).send(e));
 });
 
-//Get user by ID -> return user = json
+//Get holiday by ID -> return holiday = json
 router.get('/id/:id', (req,res) => {
-    res.send('GET holiday by ID');
+    let id = req.params['id'];
+    Holidays.findById(id).then( holiday => {
+                                if(!holiday){
+                                    return res.status(404).send();
+                                }
+                                res.send({holiday})
+                                })
+                         .catch( e => res.status(400).send());
 });
 
-//Get user by ID -> return user = json
+//update holiday by ID -> return holiday = json
 router.patch('/id/:id', (req,res) => {
-    res.send('EDIT  holiday by ID');
+    let id = req.params['id'];
+    let body = _.pick(req.body, ['startDate','endDate','holidayType','status','actionBy']);
+    body.actionDate = new Date();
+
+    Holidays.findByIdAndUpdate(id,body,{new:true}).then( holiday => {
+                                        if(!holiday){
+                                            return res.status(404).send();
+                                        }
+                                        res.send({holiday});
+                                    })
+                                    .catch( e => res.status(400).send(e));
 });
 
 // Add user to data base return 200 - user = json
 router.post('/', (req,res) => {
-    res.send('/addHoliday');
+    let body = _.pick(req.body, ['staff_id','startDate','endDate','holidayType']);
+    Users.findById(body.staff_id).then (user => {
+                                        if(!user){
+                                          return   res.status(404).send({message:'User not found!'});
+                                        }
+
+                                        let holiday = new Holidays({
+                                            staff_id:user._id,
+                                            staffName:user.name,
+                                            department: user.department,
+                                            startDate: body.startDate,
+                                            endDate: body.endDate,
+                                            holidayType: body.holidayType,
+                                            requestedDate: new Date(),
+                                        })
+
+                                        return holiday.save();
+
+                                    })
+                                  .then ( holiday => res.send({holiday}))
+                                  .catch( e => res.status(400).send(e))
 });
 
 //Delete user from Database
 router.delete('/:id', (req,res) => {
-    res.send('Delete Holiday');
+    let id = req.params['id'];
+    Holidays.findByIdAndRemove(id).then( (holiday) => {
+                                    if(!holiday){
+                                        return res.status(404).send();
+                                    }
+
+                                    res.send({holiday});
+                                })
+                                .catch( e => res.status(400).send(e));
 });
 
 
