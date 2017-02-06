782d5458d2ec6db0a0407364d71b3b5f783c179e
Ashok Siyani <ashok.siyani@gmail.com>
2017-02-06 17:40:34 +0000

    added angular material

--- public/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>
-        <p> Testing angular 2 </p>`,
-})
-export class AppComponent  { name = 'Ashok'; }
